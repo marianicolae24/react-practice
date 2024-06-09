9bddb2fe776e4250226c11dc9d7f8439b83eebc3
Corina Udrescu <corinutz@gmail.com>
2024-06-09 11:13:10 +0200

    Add dummy ToDoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import RandomQuote from "./RandomQuote";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  return <RandomQuote />;
+  return (
+    <ToDoList
+      todos={[
+        { id: 1, text: "buy milk", status: "to-do" },
+        { id: 2, text: "wash bike", status: "in-progress" },
+        { id: 3, text: "do the budget", status: "done" },
+        { id: 4, text: "call jane", status: "to-do" },
+      ]}
+    />
+  );
 }
 
 export default App;
